2b928bb71410065072c5f05b59dafeb700fbb5a8
walton2 <136533697+walton2@users.noreply.github.com>
2023-06-20 12:42:07 -0400

    Add files via upload

--- code/waller/mircro_scale.py
+++ code/waller/mircro_scale.py
@@ -5,11 +5,13 @@ import copy
 from generate_positive_definite_matrix_file import *
 from dict_of_diags_file import *
 from gehat_ge_dghat import *
-
+from scipy.sparse.csgraph import laplacian
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
 #%%     generate positive definite matrix
-A = generate_positive_definite_matrix(10)
+A = generate_positive_definite_matrix(5)
 m,n = np.shape(A)
 
 
@@ -46,30 +48,74 @@ entries_in_patterns = [m,m+m-1]
 for cc in range(m-2):
     entries_in_patterns.append( entries_in_patterns[-1] + m - 2 - cc)
 
+'''
+so this first section in here is incomplete, but can be done. might be a little 
+tricky with our dictionary approach, but certainly possible. the next takes 
+ALL adjacent patterns but idk if we want them to be a linear graph or something (only one connected to another)
 
-#%%     manually check if each set is a subset of another, and collect the index pairs of ones that are subsets
+#%%     collect the index pairs of dicts that are subsets of another manually
+#we know that each sparsity pattern is less sparse than its preceeding one
+#and can only be contained in the ones after it
 
+
+
+
+    #this may not work in python versions before 3.9
+'''
+
+
+#%%     collect the index pairs of dicts that are subsets of another by knowing pattern
+list_of_adjacents = []
+nuu =1
+for k in reversed(range(1,len(ss))):
+    for kk in range(len(ss)-nuu):
+        list_of_adjacents.append((k,kk))
+    nuu+=1
 
 
 #%%     make empty adjacency matrix
+adjacency_arr = np.zeros((n,n))
+
+#with n number of dictionaries
 
 
 #%%     with the index pairs, find difference in their sparsity as weight and store in the ajaceny matrix
 
+for k in range(len(list_of_adjacents)):
+    lef, rig = list_of_adjacents[k]
+    weightt  = 1/ (entries_in_patterns[lef] - entries_in_patterns[rig])
+    #weight is 1\distance with distance being difference in number of entries. we can
+    #normalize the distance by dividing it by n or (n/2) or something
+    adjacency_arr[lef,rig] = weightt
+
+
 
 
 #%%     calculate laplacian
+lap_arr = laplacian(adjacency_arr)
 
 
-#%%     visualize the graph????
+#%%     visualize the adjacency matrix to a graph
+
+round_adj_arr = np.around(adjacency_arr,decimals=3)
+
+
+graph_arr = nx.from_numpy_array(round_adj_arr, create_using=nx.DiGraph)
+#not sure if we want digraph or not
+layout = nx.circular_layout(graph_arr)
+#can use different layouts
+nx.draw_networkx(graph_arr,layout)
+labels = nx.get_edge_attributes(graph_arr, "weight")
+nx.draw_networkx_edge_labels(graph_arr, pos=layout, edge_labels=labels)
+plt.show()
+
+
+#%%
+
+
 
 
 #$$     minimize?????
-
-
-
-
-
 
 
 
